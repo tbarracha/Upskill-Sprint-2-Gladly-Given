f247bee996c0f9246c9f0aef4518151d9c415e42
tbarracha <ti.barracha@gmail.com>
2024-02-28 12:35:41 +0000

    Initial Empty Projects

---
+++ javaSprint2GladlyGiven/src/main/java/pt/gladlyGivenApi/GladlyGiven/GladlyGivenApplication.java
@@ -0,0 +1,13 @@
+package pt.gladlyGivenApi.GladlyGiven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GladlyGivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GladlyGivenApplication.class, args);
+	}
+
+}
---
+++ javaSprint2GladlyGiven/src/test/java/pt/gladlyGivenApi/GladlyGiven/JavaGladlyGivenApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.gladlyGivenApi.GladlyGiven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGladlyGivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
