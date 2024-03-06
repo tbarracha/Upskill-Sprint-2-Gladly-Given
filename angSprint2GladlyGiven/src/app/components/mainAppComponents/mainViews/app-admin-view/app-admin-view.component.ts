901d05707ecfb094057a12f4dc13a098eb226cab
tbarracha <ti.barracha@gmail.com>
2024-03-06 09:03:06 +0000

    added main component structure to angular project

---
+++ angSprint2GladlyGiven/src/app/components/mainAppComponents/mainViews/app-admin-view/app-admin-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-admin-view',
+  templateUrl: './app-admin-view.component.html',
+  styleUrls: ['./app-admin-view.component.scss']
+})
+export class AppAdminViewComponent {
+
+}
---
+++ angSprint2GladlyGiven/src/app/components/mainAppComponents/mainViews/app-user-view/app-user-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-user-view',
+  templateUrl: './app-user-view.component.html',
+  styleUrls: ['./app-user-view.component.scss']
+})
+export class AppUserViewComponent {
+
+}
