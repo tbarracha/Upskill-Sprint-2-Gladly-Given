4ae6053dc59e5eb959ff9c8a75a688f1e8dc8ee6
Chirano <clarissachirano13@gmail.com>
2024-03-07 11:49:58 +0000

    change project settings

--- dotNetSprint2GladyGivenWebAPI/GladyGiven/obj/Release/net7.0/GladyGivenWebAPI.AssemblyInfo.cs
+++ dotNetSprint2GladyGivenWebAPI/GladyGiven/obj/Release/net7.0/GladyGivenWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GladyGivenWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3c66be883b224ab51d546a98fc1b731408b18e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0468068e2f2a848cb439549e90676cdff5fbc603")]
 [assembly: System.Reflection.AssemblyProductAttribute("GladyGivenWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GladyGivenWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
