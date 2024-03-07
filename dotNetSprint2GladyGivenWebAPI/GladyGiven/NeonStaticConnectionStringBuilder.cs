47f16e66362ae4a2df0c15e0b0fe6166568933ad
Lia Araruna <105323436+liaararuna@users.noreply.github.com>
2024-03-07 12:14:03 +0000

    gitingore

---
+++ dotNetSprint2GladyGivenWebAPI/GladyGiven/NeonStaticConnectionStringBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+
+namespace GladyGivenWebAPI
+{
+    /// <summary>
+    /// Class to give privacy to our db connection string. Probably overkill, but it will be added to the gitIgnore so it isn't added to gitHub
+    /// </summary>
+    public static class NeonStaticConnectionStringBuilder
+    {
+        public static string GetNpgsqlConnectionString()
+        {
+            var neonBuilder = new NpgsqlConnectionStringBuilder();
+            neonBuilder.Host = "ep-yellow-sun-a20w7lo3.eu-central-1.aws.neon.tech";
+            neonBuilder.Database = "gladlyGiven";
+            neonBuilder.Username = "upskill.sprint.final";
+            neonBuilder.Password = "76nfceumUQBY";
+
+            return neonBuilder.ConnectionString;
+        }
+    }
+}
+
